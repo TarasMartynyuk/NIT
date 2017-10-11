f522475c786347d6319ff27da7591cd0ee62c258
TarasMartynyuk <tarasmartynyuk22@gmail.com>
2017-10-11 20:31:25 +0300

    Stat labels now move between bought/not bought panels on buy button click, their text is striken through First row(template) now has quantity 1 and its decrement button starts inactive

--- BuyList/code.js
+++ BuyList/code.js
@@ -2,13 +2,12 @@ $(function(){
 var template = $('.row-panel').get(0).outerHTML;
 var container = $(".goods-main-panel:first");
 //dictionary {"item name" : item stat label}
-// TODO: add var
 name_label_dict = {};
 
 //add new element to list event
 function add_new_row() {
-    console.log("cliked add button");
     var name_to_add = $("input").val();
+    $("input").val("");
     if(name_to_add in name_label_dict){
         console.warn("user tried to add a copy of existing item to the list");
         return;
@@ -36,7 +35,6 @@ $('input').keyup(function(e){
         $(this).trigger("enterKey");
 });
 $('input').bind("enterKey",function(e){
-    console.log("pressed ENTER");
     add_new_row();
  });
 
@@ -53,23 +51,29 @@ $(".shaded-button.buy-panel-button.white-font.red").click(del_row);
 
 //mark as bought
 function mark_as_bought(){
-    console.log("mark");
     var parent_row = $(this).closest(".row-panel");
     parent_row.find("p").css("text-decoration", "line-through");
     parent_row.find(".round-button").hide();
     parent_row.find(".shaded-button.buy-panel-button.white-font.red").hide();
     $(this).text("Не куплено");
+    //stat label stuff
+    item_stat_label = name_label_dict[parent_row.find("p").text()];
+    item_stat_label.find('.title').css("text-decoration", "line-through");
+    bought_label_cont.append(item_stat_label);
     $(this).unbind('click');
     $(this).click(unmark_as_bought);
 
 }
 function unmark_as_bought(){
-    console.log("unmark");
     var parent_row = $(this).closest(".row-panel");
     parent_row.find("p").css("text-decoration", "initial");
     parent_row.find(".round-button").show();
     parent_row.find(".shaded-button.buy-panel-button.white-font.red").show();
     $(this).text("Куплено");
+    //stat label stuff
+    item_stat_label = name_label_dict[parent_row.find("p").text()];
+    item_stat_label.find('.title').css("text-decoration", "initial");
+    not_bought_label_cont.append(item_stat_label);
     $(this).unbind('click');
     $(this).click(mark_as_bought);
 }
@@ -119,11 +123,13 @@ function set_inactive(button){
     button.unbind('click');
 }
 $(".round-button.red").click(decrement_quantity);
+//the first is already at quantity = 1
+set_inactive($(".round-button.red:first"));
 $(".round-button.green").click(increment_quantity);
 
 //show statistics 
 var not_bought_label_cont = $($('.row-panel.stat')[0]);
-var bought_label_cont = $($('.row-panel.stat')[1]);
+ bought_label_cont = $($('.row-panel.stat')[1]);
 var stat_label_template = $('.goods-avatar-label').get(0).outerHTML;
 not_bought_label_cont.children().map(function(i, e) {
     name_label_dict[$(e).find('.title').text()] = $(e);
